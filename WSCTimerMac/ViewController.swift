63486a98db7fa46a4cbab6ebac6b552fdcaf6347
Spoa <justinpu02@yahoo.com>
2017-12-29 11:17:56 +0800

    Initial Commit

---
+++ WSCTimerMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WSCTimerMac
+//
+//  Created by Tengoku no Spoa on 2017/12/29.
+//  Copyright © 2017年 Tengoku no Spoa. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
